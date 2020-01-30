2ae2df5ecc1f5275a014091ef2f9630176effd4c
ydtg1993 <946818508@qq.com>
2020-01-30 16:58:03 +0800

    *

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -16,8 +16,8 @@ class HomeController extends Controller
         $result = tool::curlRequest("http://baijiahao.baidu.com/builderinner/open/resource/video/publish",[
             "app_id"=>"1648637698438034",
             "app_token"=>"12105ee5e3532ed92011cf63ac23d007",
-            "title"=>"测测这个视频",
-            "video_url"=>"https://vdept.bdstatic.com/5039615352386737486b775256687559/7977734c704d794a/e6d834c7fdd8e546b050fcac46965296f67f336c527f8649f0c76acd46ee01b22df5297db75c98f4ac51341d10a6eadb.mp4?auth_key=1580381428-0-0-90728a559010853ee14d5ef5cc8af129",
+            "title"=>"测测这个视频NNNNNNNNN",
+            "video_url"=>"http://180.178.58.130/resource/image/v.mp4",
             "cover_images"=>"http://180.178.58.130/resource/image/castle.jpg",
             "is_original"=>0,
         ]);
