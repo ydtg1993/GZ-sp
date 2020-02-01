151c840a3a8184dcd999e4a4624a755ec17e3b9d
ydtg1993 <946818508@qq.com>
2020-02-01 16:14:30 +0800

    *

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -16,8 +16,8 @@ class HomeController extends Controller
         $result = tool::curlRequest("http://baijiahao.baidu.com/builderinner/open/resource/video/publish",[
             "app_id"=>"1648637698438034",
             "app_token"=>"12105ee5e3532ed92011cf63ac23d007",
-            "title"=>"测这个视频一下",
-            "video_url"=>"http://180.178.58.130/resource/vedio/v3.mp4",
+            "title"=>"全明星首发！特雷杨内突外投无所不能",
+            "video_url"=>"http://180.178.58.130/resource/vedio/vv.mp4",
             "cover_images"=>"http://180.178.58.130/resource/image/t.jpg",
             "is_original"=>0,
         ]);
