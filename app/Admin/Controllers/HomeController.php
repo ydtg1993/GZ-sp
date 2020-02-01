310820ae0c632570b5eb43241234acf22ab06d5c
ydtg1993 <946818508@qq.com>
2020-02-01 15:16:01 +0800

    *

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends Controller
             "app_id"=>"1648637698438034",
             "app_token"=>"12105ee5e3532ed92011cf63ac23d007",
             "title"=>"测测这个视频NNNNNNNNN",
-            "video_url"=>"http://180.178.58.130/resource/image/v2.mp4",
+            "video_url"=>"http://180.178.58.130/resource/vedio/v2.mp4",
             "cover_images"=>"http://180.178.58.130/resource/image/castle2.jpg",
             "is_original"=>0,
         ]);
