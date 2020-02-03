90069ece2c4080ca30786f8655f8ac957626839a
ydtg1993 <946818508@qq.com>
2020-02-03 13:03:45 +0800

    *

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -19,7 +19,7 @@ class HomeController extends Controller
         $result = tool::curlRequest("http://baijiahao.baidu.com/builderinner/open/resource/video/publish",[
             "app_id"=>"1648637698438034",
             "app_token"=>"12105ee5e3532ed92011cf63ac23d007",
-            "title"=>"40分三双！哈登面对鹈鹕砍下40分10篮板9助攻",
+            "title"=>"鹈鹕鹈鹕鹈鹕鹈鹕鹈鹕鹈鹕",
             "video_url"=>"http://180.178.58.130/resource/vedio/vm.mp4",
             "cover_images"=>"http://180.178.58.130/resource/image/t.jpg",
             "is_original"=>0,
