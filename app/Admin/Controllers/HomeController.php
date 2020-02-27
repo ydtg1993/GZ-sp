e7f9b3712a298d90665ce796a5dcb3cb16292f1a
ydtg1993 <946818508@qq.com>
2020-02-27 14:41:18 +0800

    *

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -17,16 +17,6 @@ class HomeController extends Controller
 {
     public function index(Content $content)
     {
-        /*$result = tool::curlRequest("http://baijiahao.baidu.com/builderinner/open/resource/video/publish",[
-            "app_id"=>"1648637698438034",
-            "app_token"=>"12105ee5e3532ed92011cf63ac23d007",
-            "title"=>"鹈鹕鹈鹕鹈鹕鹈鹕鹈鹕鹈鹕",
-            "video_url"=>"http://180.178.58.130/resource/vedio/vm.mp4",
-            "cover_images"=>"http://180.178.58.130/resource/image/t.jpg",
-            "is_original"=>0,
-        ]);
-        var_dump(json_decode($result,true));
-exit;*/
         return $content
             ->title('Dashboard')
             ->description('Description...')
---
+++ app/Console/Commands/ClearResource.php
@@ -0,0 +1,58 @@
+<?php
+namespace App\Console\Commands;
+
+use App\Model\VideoModel;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class ClearResource extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'clearResource';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'clear resource';
+
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $tree_days_ago = Carbon::parse('3 days ago')->toDateTimeString();
+        $videos = VideoModel::where([
+            ['created_at','<=',$tree_days_ago],
+            ['resource_status','=',0]
+        ])->get();
+        foreach ($videos as $video){
+            if (file_exists(public_path() . '/' . $video->resource)) {
+                @unlink(public_path() . '/' . $video->resource);
+            }
+            if (file_exists(public_path() . '/' . $video->resource2)) {
+                @unlink(public_path() . '/' . $video->resource2);
+            }
+            VideoModel::where('id',$video->id)->update(['resource_status'=>1]);
+        }
+    }
+}
