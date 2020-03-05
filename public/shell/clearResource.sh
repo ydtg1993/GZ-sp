d59c2c3502ed9dcf7eb0fde127d6dcbdb0ef0fc9
ydtg1993 <946818508@qq.com>
2020-03-05 10:26:11 +0800

    *

--- public/shell/clearResource.sh
+++ public/shell/clearResource.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/
-php /var/www/html/GZ-sp/artisan clearResource
+docker exec -it myphp /bin/bash -c '/usr/local/bin/php /var/www/html/GZ-sp/artisan clearResource'
--- public/shell/statistic.sh
+++ public/shell/statistic.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/
-php /var/www/html/GZ-sp/artisan clearResource
-
+docker exec -it myphp /bin/bash -c '/usr/local/bin/php /var/www/html/GZ-sp/artisan statistics'
