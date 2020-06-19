f2badd3c8be4bc7f15a432ece5a81e488b36bb68
shichuanlong <946818508@qq.com>
2020-06-19 12:58:03 +0800

    *

--- app/Admin/Controllers/VideoController.php
+++ app/Admin/Controllers/VideoController.php
@@ -98,7 +98,11 @@ EOF;
             if ($this->resource_status == 1) {
                 return '已删除';
             }
-            $source = config('app.url') . '/' . $resource;
+            if(preg_match('/^file.*/')){
+                $source = config('app.url') . '/upload/' . $resource;
+            }else {
+                $source = config('app.url') . '/' . $resource;
+            }
             return <<<EOF
 <a href="javascript:void(0);" onclick=openVideo('{$source}')>查看</a>
 <div id="{$source}"></div>
@@ -281,7 +285,11 @@ EOF;
         });
         $show->tags('标签');
         $show->resource('原视频')->unescape()->as(function ($resource) {
-            $source = config('app.url') . '/' . $resource;
+            if(preg_match('/^file.*/')){
+                $source = config('app.url') . '/upload/' . $resource;
+            }else {
+                $source = config('app.url') . '/' . $resource;
+            }
             return <<<EOF
 <video width="350" height="240" controls>
     <source src="{$source}" type="video/mp4">
@@ -375,9 +383,15 @@ EOF;
             ])->where('account_id', $account->id)->count();
             if ($limit < 5) {
                 $video = VideoModel::where('id', $videoId)->first();
-                $resource = config('app.url') . '/' . $video->resource;
+                if(preg_match('/^file.*/')){
+                    $dir = '/upload/';
+                }else{
+                    $dir = '/';
+                }
+
+                $resource = config('app.url') . $dir . $video->resource;
                 if ($toAccountType == 1) {
-                    $resource = config('app.url') . '/' . $video->resource2;
+                    $resource = config('app.url') . $dir . $video->resource2;
                 }
                 if (preg_match("/^http.*/", $video->avatar)) {
                     $avatar = $video->avatar;
